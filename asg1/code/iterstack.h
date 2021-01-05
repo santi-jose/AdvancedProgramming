6708d73a5450795443ee7d430077f63c58c334e9
santi-jose <jlsantia@ucsc.edu>
2021-01-04 21:07:36 -0800

    Added comments to iterstack.h

--- asg1/code/iterstack.h
+++ asg1/code/iterstack.h
@@ -33,14 +33,14 @@ class iterstack {
    private:
       container stack;
    public:
-      void clear() { stack.clear(); }
-      bool empty() const { return stack.empty(); }
-      size_type size() const { return stack.size(); }
-      const_iterator begin() { return stack.crbegin(); }
-      const_iterator end() { return stack.crend(); }
-      void push (const value_type& value) { stack.push_back (value); }
-      void pop() { stack.pop_back(); }
-      const value_type& top() const { return stack.back(); }
+      void clear() { stack.clear(); } //Removes all elements from the vector, leaving the container with size of 0
+      bool empty() const { return stack.empty(); } //Returns whether the vector is empty (whether size is 0)
+      size_type size() const { return stack.size(); } //Returns the number of elements in the vector, not necessarily equal to capacity
+      const_iterator begin() { return stack.crbegin(); } //Returns a const_reverse_iterator pointing to the last element in the cointainer(top of the stack)
+      const_iterator end() { return stack.crend(); } //Returns a const_reverse_iterator pointing to the theoretical element preceding the first element in the container(bottom of the stack)
+      void push (const value_type& value) { stack.push_back (value); } //Adds a new element at the end of the vector, after its current last element. Content of value is copied to the new element
+      void pop() { stack.pop_back(); } //Removes the last element in the vector, effectively reducing the container size by one
+      const value_type& top() const { return stack.back(); } //Returns a reference to the last element in the vector(top of the stack)
 };
 
 #endif
