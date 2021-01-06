58a4fb29bfa08025a12740221f1c800473fcfd8a
Jose Santiago <jlsantia@ucsc.edu>
2021-01-05 20:22:55 -0800

    Added comments describing bigint.h for my understanding

--- asg1/code/bigint.h
+++ asg1/code/bigint.h
@@ -11,31 +11,31 @@ using namespace std;
 
 #include "debug.h"
 #include "relops.h"
-#include "ubigint.h"
+#include "ubigint.h" //include ubigint.h header file
 
-class bigint {
+class bigint { //bigint class
    friend ostream& operator<< (ostream&, const bigint&);
-   private:
-      ubigint uvalue;
-      bool is_negative {false};
-   public:
+   private: //private members
+      ubigint uvalue; //declare uvalue as a ubigint
+      bool is_negative {false}; //set is_negative boolean to false
+   public: //public members
 
-      bigint() = default; // Needed or will be suppressed.
-      bigint (long);
-      bigint (const ubigint&, bool is_negative = false);
-      explicit bigint (const string&);
+      bigint() = default; // Need default constructor or will be suppressed.
+      bigint (long); //bigint constructor with long input
+      bigint (const ubigint&, bool is_negative = false); //bigint constructor with ubigint pointer input and is_negative boolean set to false
+      explicit bigint (const string&); //explicit bigint constructor with string pointer input
 
       bigint operator+() const;
       bigint operator-() const;
 
-      bigint operator+ (const bigint&) const;
-      bigint operator- (const bigint&) const;
-      bigint operator* (const bigint&) const;
-      bigint operator/ (const bigint&) const;
-      bigint operator% (const bigint&) const;
+      bigint operator+ (const bigint&) const; //operator+ function prototype: adds two bigint's 
+      bigint operator- (const bigint&) const; //operator- function prototype: subtracts two bigint's
+      bigint operator* (const bigint&) const; //operator* function prototype: multiplies two bigint's
+      bigint operator/ (const bigint&) const; //operator/ function prototype: divides two bigint's
+      bigint operator% (const bigint&) const; //operator% function prototype: find the remainder of the quotient of two bigint's
 
-      bool operator== (const bigint&) const;
-      bool operator<  (const bigint&) const;
+      bool operator== (const bigint&) const; //operator== function prototype: checks equality of two bigint's
+      bool operator<  (const bigint&) const; //operator< function prototype: checks if one bigint is less than the other
 };
 
 #endif
