7dfa68556f16ee5e6e9908f09fb844b8656a421a
Jose Santiago <jlsantia@ucsc.edu>
2021-01-10 00:31:44 -0800

    Edited comments to file ubigint.h

--- asg1/code/ubigint.h
+++ asg1/code/ubigint.h
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <limits>
 #include <utility>
+#include <cstdint> //include <cstdint> to use uint8_t
+#include <vector> //include <vector> library
 using namespace std;
 
 #include "debug.h"
@@ -14,24 +16,24 @@ using namespace std;
 class ubigint { //ubigint class
    friend ostream& operator<< (ostream&, const ubigint&);
    private: //private members
-      using ubigvalue_t = vector<uint8_t>; //ubigvalue_t declared as vector of uint8_t
+      using ubigvalue_t = vector<uint8_t>; //ubigvalue_t defined as vector<uint8_t>
       ubigvalue_t ubig_value; //create ubig_value of type ubigvalue_t(vector<uint8_t>)
    public: //public members
-      void multiply_by_2(); //ubigint.multiply_by_2(): multiplies ubigint by 2
-      void divide_by_2(); //ubigint.divide_by_2(): divides ubigint by 2
+      void multiply_by_2(); //multiplies ubigint by 2
+      void divide_by_2(); //divides ubigint by 2
 
       ubigint() = default; // Need default constructor as well
-      ubigint (unsigned long); //ubigint constructor with input of unsigned long
-      ubigint (const string&); //ubigint constructor with input of string pointer
+      ubigint (unsigned long); //ubigint constructor from unsigned long
+      ubigint (const string&); //ubigint constructor from string&(address/reference)
 
-      ubigint operator+ (const ubigint&) const; //operator+ function prototype: adds two ubigint's
-      ubigint operator- (const ubigint&) const; //operator- function prototype: subtracts two ubigint's
-      ubigint operator* (const ubigint&) const; //operator* function prototype: multiplies two ubigint's
-      ubigint operator/ (const ubigint&) const; //operator/ function prototype: divides two ubigint's
-      ubigint operator% (const ubigint&) const; //operator% function prototype: finds the remainder of the division of two ubigint's
+      ubigint operator+ (const ubigint&) const; //adds two ubigint's
+      ubigint operator- (const ubigint&) const; //subtracts two ubigint's
+      ubigint operator* (const ubigint&) const; //multiplies two ubigint's
+      ubigint operator/ (const ubigint&) const; //divides two ubigint's
+      ubigint operator% (const ubigint&) const; //finds remainder of division of two ubigint's
 
-      bool operator== (const ubigint&) const; //operator== function prototype: checks for equality of two ubigint's
-      bool operator<  (const ubigint&) const; //operator< function prototype: checks if one ubigint is less than another
+      bool operator== (const ubigint&) const; //checks for equality of two ubigint's
+      bool operator<  (const ubigint&) const; //checks if one ubigint is less than other
 };
 
 #endif
