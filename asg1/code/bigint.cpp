8747b67662485b48b5b97a298f4188c6debb77f7
Jose Santiago <jlsantia@ucsc.edu>
2021-01-12 23:39:44 -0800

    Finished operator+ with unsigned long implementation

--- asg1/code/bigint.cpp
+++ asg1/code/bigint.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #include "bigint.h"
 
 bigint::bigint (long that): uvalue (that), is_negative (that < 0) { //bigint constructor
-   //DEBUGF ('~', this << " -> " << uvalue) //causes problems until ubigint operator<< is implemented
+   DEBUGF ('~', this << " -> " << uvalue) //causes problems until ubigint operator<< is implemented
 }
 
 bigint::bigint (const ubigint& uvalue_, bool is_negative_): //bigint constructor
@@ -28,10 +28,23 @@ bigint bigint::operator+ () const {
 bigint bigint::operator- () const {
    return {uvalue, not is_negative};
 }
-/*
+
 bigint bigint::operator+ (const bigint& that) const { //sum of bigint's
-   ubigint result = uvalue + that.uvalue;
-   return result;
+   if(that.is_negative == is_negative){ //if signs are equal
+      ubigint result = uvalue + that.uvalue; //add ubigint's
+	  return bigint(result, is_negative); //return bigint
+   }else{ //operands are of different sign
+	   if(uvalue > that.uvalue){ //if uvalue has more digits than that.uvalue
+		   ubigint result = uvalue - that.uvalue; //subtract that.uvalue from uvalue
+		   return bigint(result, is_negative); //return bigint with result and is_negative
+	   }else if(that.uvalue > uvalue){ //else if that.uvalue has more digits than uvalue
+		   ubigint result = that.uvalue - uvalue; //subtract uvalue from that.uvalue
+		   return bigint(result, that.is_negative); //return bigint with result and that.is_negative 
+	   }else{ //else uvalue and that.uvalue are the same
+		   ubigint result = that.uvalue - uvalue;
+		   return bigint(result, false);
+	   }
+   }
 }
 
 bigint bigint::operator- (const bigint& that) const { //difference of bigint's
@@ -64,7 +77,7 @@ bool bigint::operator< (const bigint& that) const { //less than boolean of bigin
    return is_negative ? uvalue > that.uvalue
                       : uvalue < that.uvalue;
 }
-*/
+
 ostream& operator<< (ostream& out, const bigint& that) {
    return out << "bigint(" << (that.is_negative ? "-" : "+")
               << "," << that.uvalue << ")";
