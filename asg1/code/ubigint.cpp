2d41cbbd04b6f7d89d6d9e37961a613f83d789e2
Jose Santiago <jlsantia@ucsc.edu>
2021-01-24 23:44:06 -0800

    Final version completed for submission

--- asg1/code/ubigint.cpp
+++ asg1/code/ubigint.cpp
@@ -11,54 +11,143 @@ using namespace std;
 #include "relops.h"
 #include "ubigint.h"
 
-ubigint::ubigint (unsigned long that): ubig_value (that){ //unsigned long implementation
-   DEBUGF ('~', this << " -> " << ubig_value)
-/*ubigint::ubigint (unsigned long that) { //ubigint constructor
-   DEBUGF ('~', this << " -> " << ubig_value)
-   while(that != 0){ //while that is non-zero
-      ubig_value.push_back(that%10); //push least significant digit from 0-9 into ubig_value
-      that = that/10; //divide that by 10 to evaluate next significant digit in that
-   }*/
+//ubigint constructor w/ unsigned long input
+ubigint::ubigint (unsigned long that) { 
+   DEBUGF ('~', this << " -> " /*<< ubig_value*/)
+   //while that is non-zero
+   if(that!=0){
+      while(that != 0){ 
+         //push least significant digit
+         ubig_value.push_back(that%10); 
+         //divide that by 10 to evaluate next significant digit
+         that = that/10;
+      }
+      del_zeroes(); //delete leading zeroes
+   }
 }
 
-ubigint::ubigint (const string& that): ubig_value(0) { //ubigint constructor
-   DEBUGF ('~', "that = \"" << that << "\"");
-   for (char digit: that) { // unsigned long implementation
-      if (not isdigit (digit)) {
-         throw invalid_argument ("ubigint::ubigint(" + that + ")");
-      }
-      ubig_value = ubig_value * 10 + digit - '0';
-   }
-   /*for (char digit: that) { //iterate through chars in string& that
-      if (not isdigit (digit)) { //if the char is not a digit
-         throw invalid_argument ("ubigint::ubigint(" + that + ")"); //throw error
+//ubigint constructor w/ string input
+ubigint::ubigint (const string& that) {
+   DEBUGF ('~', "that = \"" << that << "\"");   
+   //iterate through chars in string& that
+   for (char digit: that) { 
+      //if the char is not a digit
+      if (not isdigit (digit)) { 
+         //throw error
+         throw invalid_argument ("ubigint::ubigint(" + that + ")"); 
       } //otherwise
-      ubig_value.push_back(digit - '0'); //push conversion of char to ubig_value(digit - '0')
-   }*/
+      //push conversion of char to ubig_value(digit - '0')
+      ubig_value.insert(ubig_value.begin() ,digit - '0'); 
+   }
+   del_zeroes(); //delete leading zeroes
 }
 
 ubigint ubigint::operator+ (const ubigint& that) const {
-   DEBUGF ('u', *this << "+" << that);
-   ubigint result (ubig_value + that.ubig_value);
-   DEBUGF ('u', result);
-   return result;
+   //DEBUGF ('u', *this << "+" << that);
+   ubigint result; //define ubigint to store sum
+   uint8_t digit1; //declare digit1
+   uint8_t digit2; //declare digit2
+   uint8_t sum; //declare sum
+   uint8_t carry = 0; //initialize carry to zero
+   
+   //iterate through this and that ubig_value
+   for(long unsigned int i = 0;
+      (i<ubig_value.size())or(i<that.ubig_value.size());
+      ++i){
+      //assign digit1 and digit2
+      digit1 = i < ubig_value.size() ? ubig_value[i] : 0;
+      digit2 = i < that.ubig_value.size() ? that.ubig_value[i] : 0;
+      //assign sum
+      sum = (digit1 + digit2 + carry)%10; 
+      //assign carry
+      carry = (digit1 + digit2 + carry)/10; 
+      //push sum into ubig_value
+      result.ubig_value.push_back(sum); 
+   }
+   
+   //if we have any other carry values lingering
+   if(carry > 0){ 
+      //push remaining carry on stack
+      result.ubig_value.push_back(carry); 
+   }
+   result.del_zeroes(); //delete leading zeroes
+   return result; //return result
+   //DEBUGF ('u', result);
 }
 
 ubigint ubigint::operator- (const ubigint& that) const {
    if (*this < that) throw domain_error ("ubigint::operator-(a<b)");
-   return ubigint (ubig_value - that.ubig_value);
+   ubigint result; //define ubigint to store difference
+   int digit1; //declare digit1
+   int digit2; //declare digit2
+   int diff; //declare diff
+   int borrow = 0; //initialize borrow
+   
+   //iterate through this and that ubig_value
+   for(long unsigned int i = 0;
+      (i<ubig_value.size())or(i<that.ubig_value.size());
+      ++i){
+      digit1 = i < ubig_value.size() ? ubig_value[i] : 0;
+      digit2 = i < that.ubig_value.size() ? that.ubig_value[i] : 0;
+      if((digit1 - borrow) < digit2){
+         diff = (digit1 + 10) - digit2 - borrow;
+         borrow = 1; 
+      }else{
+         diff = digit1 - digit2 - borrow;
+         borrow = 0;
+      }
+      result.ubig_value.push_back(uint8_t(diff));
+   }
+   result.del_zeroes(); //delete leading zeroes
+   return result; //return result
 }
 
 ubigint ubigint::operator* (const ubigint& that) const {
-   return ubigint (ubig_value * that.ubig_value);
+   //initialize variables
+   ubigint result; //define ubigint to store product
+   //initialize result.ubig_value to size of n*m
+   for(long unsigned int k = 0; 
+      k < ((ubig_value.size()+that.ubig_value.size()));
+      ++k){
+      result.ubig_value.push_back(0);
+   }
+   uint8_t prod; //declare digit to store into result
+   uint8_t carry; //declare carry
+   
+   //multiply operands if they are non-zero
+   for(long unsigned int i = 0;i<ubig_value.size();++i){
+      carry = 0;
+      for(long unsigned int j = 0;j<that.ubig_value.size();++j){
+         prod = result.ubig_value[i+j]
+              + ubig_value[i]*that.ubig_value[j]
+              + carry;
+         result.ubig_value[i+j] = prod % 10;
+         carry = prod / 10;
+      }
+      result.ubig_value[i+(that.ubig_value.size())] = carry;
+   }
+   result.del_zeroes(); //trim leading zeroes
+   return result;
 }
 
 void ubigint::multiply_by_2() {
-   ubig_value *= 2;
+   (*this)=(*this) * 2;
 }
 
 void ubigint::divide_by_2() {
-   ubig_value /= 2;
+   for(long unsigned int i = 0;i < ubig_value.size(); ++i){
+      if((i+1)<ubig_value.size()){ //there is a next sig dig
+         //check if next digit is odd or even
+         if((ubig_value[i+1] % 2) == 1){ //next digit is odd
+            ubig_value[i] = (ubig_value[i] / 2) + 5;
+         }else{ //next digit is even
+            ubig_value[i] = ubig_value[i] / 2;
+         }
+      }else{ //this is the most significant digit
+         ubig_value[i] = ubig_value[i] / 2;
+      }
+   }
+   del_zeroes(); //trim leading zeroes
 }
 
 
@@ -83,8 +172,8 @@ quo_rem udivide (const ubigint& dividend, const ubigint& divisor_) {
       divisor.divide_by_2();
       power_of_2.divide_by_2();
    }
-   DEBUGF ('/', "quotient = " << quotient);
-   DEBUGF ('/', "remainder = " << remainder);
+   //DEBUGF ('/', "quotient = " << quotient);
+   //DEBUGF ('/', "remainder = " << remainder);
    return {.quotient = quotient, .remainder = remainder};
 }
 
@@ -97,20 +186,56 @@ ubigint ubigint::operator% (const ubigint& that) const {
 }
 
 bool ubigint::operator== (const ubigint& that) const {
-   return ubig_value == that.ubig_value;
+   if(ubig_value == that.ubig_value){ //vectors are equal
+      return true; //return true
+   }else{ //vectors are not equal
+      return false; //return false
+   }
 }
 
 bool ubigint::operator< (const ubigint& that) const {
-   return ubig_value < that.ubig_value;
+   if(ubig_value.size() < that.ubig_value.size()){ 
+      return true;
+   }else if(ubig_value.size() > that.ubig_value.size()){ 
+      return false;
+   }else{ //ubig_value.size() == that.ubig_value.size()
+      //iterate through vectors from back to front
+      for(int i = ubig_value.size() - 1; i >= 0; --i){ 
+         if(ubig_value[i] < that.ubig_value[i]){ 
+            return true;
+         }else if(ubig_value[i] > that.ubig_value[i]){
+            return false;
+         }
+         //elements must be equal, check next less significant element
+      } //vectors must be equal
+      return false; //checked all members of vectors, therefore false
+   }
 }
 
 ostream& operator<< (ostream& out, const ubigint& that) { 
-   return out << "ubigint(" << that.ubig_value << ")"; // unsigned long implementation
-   /*out << "ubigint(";
-   for(auto it = that.ubig_value.rbegin(); it != that.ubig_value.rend(); ++it){ //iterate ubigint from end to begin
-      out << int(*it); //send element from ubigint at iterator to out
+   //iterate ubigint from end to begin
+   int char_count = 0;
+
+   if(that.ubig_value.size()==0){
+      out << 0;
+   }else{
+      for(auto it = that.ubig_value.rbegin(); 
+         it != that.ubig_value.rend(); 
+         ++it){
+         out << int(*it); //send element from ubigint at iterator to out
+         char_count++; //increment char count
+         if(char_count%69 == 0){
+            out<<"\\"<<endl;
+         }
+      }
    }
-   out << ")"; //close bracket for current ubigint
-   return out;*/
+   return out;
+}
+
+//del_zeroes helper function that deletes leading zeroes
+void ubigint::del_zeroes(){
+   while((ubig_value.back()==0) and (ubig_value.size()>0)){
+      ubig_value.pop_back(); //delete leading zero
+   }
 }
 
